b9ea39b3525f215ed2585fc2cb5ab4da848cceb2
Alexander Gude <alex.public.account+pathfinderhelpers@gmail.com>
2012-12-18 18:40:09 -0600

    Add option to use a starting list to MarkovChainName.py

--- MarkovChainName.py
+++ MarkovChainName.py
@@ -21,6 +21,7 @@ class MarkovChain:
     """ Class to store weighted Markov Chain data """
     def __init__(self):
         self.d = {}
+        self.s = []
 
     def __getitem__(self, prefix):
         """ Return a suffix for a given input prefix """
@@ -32,6 +33,12 @@ class MarkovChain:
             self.d[prefix].append(suffix)
         else:
             self.d[prefix] = [suffix]
+
+    def addStart(self, prefix, suffix):
+        """ Add a prefix, and the suffix, to both the normal list and starts
+        list """
+        self.s.append(prefix)
+        self.add(prefix, suffix)
             
     def keys(self):
         """ Return keys """
@@ -43,15 +50,17 @@ class MarkovChain:
 
 class NameGenerator:
     """ Use a Markov Chain and a list of input names to generate random names """
-    def __init__(self, inputFile, chainLength, maxLength, noDupes):
+    def __init__(self, inputFile, chainLength, maxLength, noDupes, useStarts):
         self.inputFile = inputFile
         self.chainLength = chainLength
         self.maxLength = maxLength
         self.noDupes = noDupes
+        self.useStarts = useStarts
         self.mc = MarkovChain()
         self.__loadFile()
         self.__parseData()
         #print(self.mc)
+        #print(self.mc.s)
 
     def __loadFile(self):
         """ Load names from input file into a set """
@@ -66,22 +75,36 @@ class NameGenerator:
         """ Run through self.data, parse it, and store the results into
         MarkovChain object """
         for word in self.data:
+            firstRun = True
             while len(word) >= self.chainLength:
                 prefix = word[0:self.chainLength]
                 word = word[self.chainLength:]
+                # First run
                 # Check to see if we're at the end of our word
                 if len(word) == self.chainLength:
                     suffix = word[0:self.chainLength]
-                    self.mc.add(prefix,suffix)
+                    if firstRun:
+                        self.mc.addStart(prefix,suffix)
+                    else:
+                        self.mc.add(prefix,suffix)
                     self.mc.add(suffix,'\n')
                     break
+                # Check to see if we are at the end, and need to pad
                 elif len(word) < self.chainLength:
                     suffix = word[0:self.chainLength] + '\n'
-                    self.mc.add(prefix,suffix)
+                    if firstRun:
+                        self.mc.addStart(prefix,suffix)
+                    else:
+                        self.mc.add(prefix,suffix)
                     break
+                # Otherwise keep running
                 else:
                     suffix = word[0:self.chainLength]
-                    self.mc.add(prefix,suffix)
+                    if firstRun:
+                        self.mc.addStart(prefix,suffix)
+                    else:
+                        self.mc.add(prefix,suffix)
+                firstRun = False
 
     def makeNames(self,nnames):
         """ Generate n names """
@@ -90,7 +113,12 @@ class NameGenerator:
         i = 0
         while len(self.names) < nnames and i <= nnames * 100:
             i += 1 # Makes it so we don't run forever
-            prefix = choice(tuple(self.mc.keys()))
+            # Generate first part of name, use only the start list in mc class
+            # if useStart is set
+            if self.useStarts:
+                prefix = choice(tuple(self.mc.s))
+            else:
+                prefix = choice(tuple(self.mc.keys()))
             name = prefix
             # Walk through the chain for each name
             while True:
@@ -129,17 +157,18 @@ if __name__ == '__main__':
     let functions use them.  """
 
     usage = "usage: %prog [Options]"
-    version = "%prog Version 1.0.0\n\nCopyright (C) 2012 Alexander Gude - alex.public.account+pathfinderhelper@gmail.com\nThis is free software.  You may redistribute copies of it under the terms of\nthe GNU General Public License <http://www.gnu.org/licenses/gpl.html>.\nThere is NO WARRANTY, to the extent permitted by law.\n\nWritten by Alexander Gude."
+    version = "%prog Version 1.1.0\n\nCopyright (C) 2012 Alexander Gude - alex.public.account+pathfinderhelper@gmail.com\nThis is free software.  You may redistribute copies of it under the terms of\nthe GNU General Public License <http://www.gnu.org/licenses/gpl.html>.\nThere is NO WARRANTY, to the extent permitted by law.\n\nWritten by Alexander Gude."
     parser = OptionParser(usage=usage,version=version)
     parser.add_option("-f", "--input-file", action="store", type="str", dest="inputFile", help="input file containing a list of names, one per line")
     parser.add_option("-c", "--chain-length", action="store", type="int", dest="chainLength", default=2, help="length of fragments [default 2]")
     parser.add_option("-m", "--max-length", action="store", type="int", dest="maxLength", default=9, help="maximum length of a name [default 9]")
     parser.add_option("-n", "--n-names", action="store", type="int", dest="nNames", default=5, help="create this many names [default 5]")
     parser.add_option("-i", "--not-in-input", action="store_true", dest="noDupes", default=False, help="prevent generating names found in the input file [default false]")
+    parser.add_option("-s", "--use-starts", action="store_true", dest="useStarts", default=False, help="start names only with combinations that also start names in the input file [default false]")
     parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="print status messages to stdout [default false]")
     parser.add_option("-q", "--quite", action="store_false", dest="verbose", default=False, help="do not print status messages to stdout")
 
     (options, args) = parser.parse_args()
 
-    ng = NameGenerator(options.inputFile, options.chainLength, options.maxLength, options.noDupes)
+    ng = NameGenerator(options.inputFile, options.chainLength, options.maxLength, options.noDupes, options.useStarts)
     ng.makeNames(options.nNames)
