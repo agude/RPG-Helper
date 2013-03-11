2e0e839e8457b7aae9601ad9bbe950e3d0d4fa4e
Alexander Gude <alex.public.account+pathfinderhelpers@gmail.com>
2013-03-11 11:04:49 -0700

    Adding in beginnings of DateHandler.

---
+++ DateHandler.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python3
+#  Copyright (C) 2013  Alexander Gude - alex.public.account+pathfinderhelper@gmail.com
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+from math import floor
+
+class Date:
+    """ Given a date, calculates all relevant information. """
+    def __init__(self, inDate):
+        """ Take a date and parse it. Dates are over the form 0 <= x <= 360, or "Season Day" """
+        self.date = None
+        self.season = None
+        self.sday = None
+        try:
+            self.date = int(inDate)
+        except ValueError:
+            tmpseason = inDate.split(' ')[0]
+            if tmpseason.lower() in ['spring', 'summer', 'fall', 'winter']:
+                self.season = tmpseason.capitalize()
+                self.sday = int(inDate.split(' ')[1])
+
+        if self.date:
+            self.__parseDate()
+        elif self.season and self.sday:
+            self.__parseSDay()
+
+        self.__extractDOW()
+
+    def __parseSDay(self):
+        """ Parse self.sday and self.season and extract all information """
+        # Extract date
+        seasondic = {
+                "Summer":90,
+                "Spring":0,
+                "Fall":180,
+                "Winter":270
+                }
+        mod = seasondic[self.season]
+        self.date = self.sday + mod
+
+        print(self.date)
+
+    def __parseDate(self):
+        """ Parse self.date and extract all information """
+        # Extract Season and sday
+        if self.date <= 90:
+            self.season = "Spring"
+            self.sday = self.date 
+        elif self.date <= 180:
+            self.season = "Summer"
+            self.sday = self.date - 90
+        elif self.date <= 270:
+            self.season = "Fall"
+            self.sday = self.date - 180
+        elif self.date > 270:
+            self.season = "Winter"
+            self.sday = self.date - 270
+
+    def __extractDOW(self):
+        """ Calculate the day of the week """
+        if self.date%5 == 1:
+            self.dow = "1st"
+        elif self.date%5 == 2:
+            self.dow = "2nd"
+        elif self.date%5 == 3:
+            self.dow = "3rd"
+        elif self.date%5 == 4:
+            self.dow = "4th"
+        else:
+            self.dow = "The Day of Rest"
+
+    def __repr__(self):
+        """ Used for interactive display and printing """
+        if self.dow != "The Day of Rest":
+            return "It is {0} {1}, which is the {2} day of the year, and the {3} day of the week.".format(
+                    self.season,
+                    self.sday,
+                    self.date,
+                    self.dow
+                    )
+        else:
+            return "It is {0} {1}, which is the {2} day of the year, and also {3}.".format(
+                    self.season,
+                    self.sday,
+                    self.date,
+                    self.dow
+                    )
+               
+
+    
+
+##### START OF CODE
+if __name__ == '__main__':
+
+    from optparse import OptionParser # Command line parsing
+
+    """ Allows command line options to be parsed. Called first to in order to
+    let functions use them.  """
+
+    usage = "usage: %prog [Options]"
+    version = "%prog Version 1.0.0\n\nCopyright (C) 2013 Alexander Gude - alex.public.account+pathfinderhelper@gmail.com\nThis is free software.  You may redistribute copies of it under the terms of\nthe GNU General Public License <http://www.gnu.org/licenses/gpl.html>.\nThere is NO WARRANTY, to the extent permitted by law.\n\nWritten by Alexander Gude."
+    parser = OptionParser(usage=usage,version=version)
+    parser.add_option("-f", "--first-name-list", action="store", type="str", dest="fnList", default=None, help="input file containing a list of first names")
+    parser.add_option("-l", "--last-name-list", action="store", type="str", dest="lnList", default=None, help="input file containing a list of last names")
+    parser.add_option("-p", "--hyphen-probability", action="store", type="float", dest="hyphenP", default=".05", help="number in [0.,1.] giving the probability of hyphenating a last name.")
+    parser.add_option("-n", "--n-names", action="store", type="int", dest="nNames", default="10", help="print this number of names")
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="print status messages to stdout [default false]")
+    parser.add_option("-q", "--quite", action="store_false", dest="verbose", default=False, help="do not print status messages to stdout")
+
+    (options, args) = parser.parse_args()
